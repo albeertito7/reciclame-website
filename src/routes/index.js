d1b64945906e3b2725fec53aa58c9bbcc8848cbe
albeertito7 <albertperezdatsira@gmail.com>
2021-01-13 14:09:02 +0100

    index routes code cleaned

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const router = Router();
 const admin = require("firebase-admin");
 const {AlphaAnalyticsDataClient} = require('@google-analytics/data');
 const serviceAccountKey = JSON.parse(Buffer.from(process.env.FIREBASE_APP_CREDENTIALS, 'base64')); //require("../../serviceAccountKey.json");
-//const firestore = require("firebase/firestore");
 
 /* connection string */
 admin.initializeApp({
@@ -12,224 +11,204 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-
-const projectId = 'reciclame-app-b1f20';
+const propertyId = 252994745, projectId = 'reciclame-app-b1f20';
 const client = new AlphaAnalyticsDataClient({keyFilename: "serviceAccountKey.json"});
 
 router.get('/', (req, res) => {
+	console.log("get /home");
 	res.render('index', { title: 'Reciclame' });
 });
 
 router.get('/about-us', (req, res) => {
-	res.render('about', { title: 'About Us Page' });
+	console.log("get /about-us");
+	res.render('about', { title: 'About us page' });
 });
 
 router.get('/contact', (req, res) => {
-	res.render('contact', { title: 'Contact Page' });
+	console.log("get /contact");
+	res.render('contact', { title: 'Contact page' });
 });
 
 router.get('/admin', checkCookieMiddleware, (req, res) => {
-	console.log("router /admin");
+	console.log("get /admin");
 	res.redirect("/profile");
 });
 
-router.post("/sessionLogin", (req, res) => {
-  console.log("sessionLogin post");
+router.post("/login", (req, res) => {
+  	console.log("post /login");
 
-  const idToken = req.body.idToken.toString(),
-        expiresIn = 60 * 60 * 24 * 1 * 1000;
+	const idToken = req.body.idToken.toString(),
+			expiresIn = 60 * 60 * 24 * 1 * 1000; // 1 day
 
-  admin
-    .auth()
-    .createSessionCookie(idToken, { expiresIn })
-    .then(
-      (sessionCookie) => {
-        console.log("cookie created: " + sessionCookie);
-        const options = { maxAge: expiresIn, httpOnly: true }; // secure: true
-		res.cookie('session', sessionCookie, options);
-		
-		admin.auth().verifyIdToken(idToken).then(function(decodedClaims) {
-			res.redirect('/profile');
-			res.end(JSON.stringify({ status: "success" }));
-		});
-      },
-      (error) => {
-        console.log("cookie creation error unauthorized request");
-        res.status(401).send("UNAUTHORIZED REQUEST!");
-      }
-    );
+	admin
+		.auth()
+		.createSessionCookie(idToken, { expiresIn })
+		.then(
+		(sessionCookie) => {
+			console.log("Cookie created: " + sessionCookie);
+			const options = { maxAge: expiresIn, httpOnly: true }; // secure: true
+			res.cookie('session', sessionCookie, options);
+			
+			admin
+				.auth()
+				.verifyIdToken(idToken)
+				.then((decodedClaims) => {
+					console.log("/login: User logged: " + JSON.stringify(decodedClaims));
+					res.status(200).end();
+			});
+		},
+		(error) => {
+			console.log("/login: Cookie creation error: " + error);
+			res.status(401).send("UNAUTHORIZED REQUEST!");
+		}
+	);
 });
 
-router.get("/sessionLogout", (req, res) => {
-	res.clearCookie("session");
-	res.render("login");
+router.get("/logout", (req, res) => {
+	console.log("get /logout");
+	res.clearCookie("session").render("login");
 });
 
 router.get("/profile", checkCookieMiddleware, (req, res) => {
-	console.log("router /profile");
+	console.log("get /profile");
 	let uid = req.decodedClaims.uid;
-	console.log("decodeClaims: " + JSON.stringify(req.decodedClaims));
-	console.log("UID: " + uid);
 
 	db.collection('userData').doc(uid).get()
 	.then(snapshot => {
 		if(snapshot.empty) {
-			console.log("No matching documents")
+			console.log("/profile: No matching user documents");
 			res.render("login");
 		} else {
 			let data = snapshot.data();
-			console.log(data);
 			db.collection('userData').get()
 			.then(snapshot => {
-				/*let totalUsers = 0, adminUsers = 0;
-				snapshot.docs.forEach(user => {
-					if(user.data().isAdmin) {
-						adminUsers++;
-					}
-					totalUsers++;
-				});*/
 				res.render("profile", {
-					'aud': req.decodedClaims.aud,
 					'uid': uid,
+					'aud': req.decodedClaims.aud,
 					'email': req.decodedClaims.email,
 					'emailVerified': req.decodedClaims.email_verified,
 					'fullName': data.fullName,
-					//'city': data.city,
-					//'postalCode': data.postalCode,
-					/*'totalUsers': totalUsers,
-					'adminUsers': adminUsers,
+					/*'city': data.city,
+					'postalCode': data.postalCode,
 					'isAdmin': data.isAdmin*/
 				});
 			});
 		}
 	})
 	.catch(err => {
-		console.log("Error getting docuemnts: " + err);
-		res.status(500).send("Error getting user data!");
+		console.log("/profile: Error getting user documents: " + err);
+		res.status(500).send("Error getting user documents!");
 	});
 });
 
 router.get("/editProfile", checkCookieMiddleware, (req, res) => {
-	console.log("router /editProfile");
-
+	console.log("get /editProfile");
 	let uid = req.decodedClaims.uid;
-	console.log("UID: " + uid);
 
 	db.collection('userData').doc(uid).get()
 	.then(snapshot => {
 		if(snapshot.empty) {
-			console.log("No matching documents")
+			console.log("/editProfile: No matching user documents");
 			res.render("login");
 		} else {
 			let data = snapshot.data();
-			console.log(data);
 			res.render("editProfile", {
 				'fullName': data.fullName,
-				//'email': req.decodedClaims.email,
 				'city': data.city,
 				'postalCode': data.postalCode
 			});
 		}
 	})
 	.catch(err => {
-		console.log("Error getting documents: " + err);
-		res.status(500).send("Error getting user data!");
+		console.log("/editProfile: Error getting user document: " + err);
+		res.status(500).send("Error getting user document!");
+	});
+});
+
+router.post("/saveUserData", verifySession, (req, res) => {
+	console.log("post /saveUserData");
+
+	db.collection("userData").doc(req.decodedClaims.uid).set({
+		fullName: req.body.fullName,
+		city: req.body.city,
+		postalCode: req.body.postalCode
+	})
+	.then(() => {
+		console.log("/saveUserData: User data document correctly updated");
+		res.end(JSON.stringify({ status: "success" }));
+	})
+	.catch((error) => {
+		console.log("/saveUserData: Error updating user data document: " + error);
+		res.status(500).send("Error updating user data document!");
 	});
 });
 
 router.get('/createUser', checkCookieMiddleware, (req, res) => {
-	console.log("router /createUser");
+	console.log("get /createUser");
 	res.render('signup');
 });
 
-router.post("/saveUserData", verifySession, (req, res) => {
-	console.log("createUserData post");
-
-  /*admin.auth().updateUser(req.decodedClaims.uid, {
-	  email: req.body.email
-  })
-  .then(function(userRecord) {})
-  .catch(function (error) {
-	  console.log('Error updating user: ' + error);
-	  res.status(500).send("Error Ocurred!");
-  });*/
-
-  db.collection("userData").doc(req.decodedClaims.uid).set({
-	  fullName: req.body.fullName,
-	  city: req.body.city,
-	  postalCode: req.body.postalCode
-  })
-  .then(function() {
-	  console.log("UserData document correctly updated");
-	  res.end(JSON.stringify({ status: "success" }));
-  })
-  .catch(function (error) {
-	  console.log("Error updating the userData document: " + error);
-	  res.status(500).send("Error Ocurred!");
-  });
-});
-
 router.post("/createUserData", (req, res) => {
-  console.log("createUserData post");
-  console.log(req.body);
+  console.log("post /createUserData");
 
 	admin
-	.auth()
-	.createUser({
-		email: req.body.email,
-		emailVerified: false,
-		password: req.body.password,
-		emailVerified: req.body.emailVerified,
-		disabled: req.body.disabled
-	})
-	.then((userRecord) => {
-		db.collection("userData").doc(userRecord.uid).set({
-			avatarRef: "default",
-			city: req.body.city,
-			enable: true,
-			fullName: req.body.fullName,
-			isAdmin: req.body.isAdmin,
-			postalCode: req.body.postalCode,
-			score: 0,
-			uid: userRecord.uid
-		  })
-		  .then(function() {
-			console.log("UserData document correctly set");
-			res.end(JSON.stringify({ status: "success" }));
-		  })
-		  .catch(function (error) {
-			console.log("Error creating the userData document: " + error);
-			res.status(500).send("Error Ocurred!");
-		  });
-	})
-	.catch((error) => {
-		console.log("Error creating the userData document: " + error);
-		res.status(500).send("Error Ocurred!");
-	});
+		.auth()
+		.createUser({
+			email: req.body.email,
+			emailVerified: false,
+			password: req.body.password,
+			emailVerified: req.body.emailVerified,
+			disabled: req.body.disabled
+		})
+		.then((userRecord) => {
+			db.collection("userData").doc(userRecord.uid).set({
+				avatarRef: "default",
+				city: req.body.city,
+				enable: true,
+				fullName: req.body.fullName,
+				isAdmin: req.body.isAdmin,
+				postalCode: req.body.postalCode,
+				score: 0,
+				uid: userRecord.uid
+			})
+			.then(function() {
+				console.log("/createUserData: User data document correctly set");
+				res.end(JSON.stringify({ status: "success" }));
+			})
+			.catch(function (error) {
+				console.log("/createUserData: Error creating the user data document: " + error);
+				res.status(500).send("Error creating user data document!");
+			});
+		})
+		.catch((error) => {
+			console.log("Error creating user: " + error);
+			res.status(500).send("Error creating user!");
+		});
 
 });
 
 router.post("/updateCredentials", verifySession, (req, res) => {
-	console.log("updateCredentials post");
+	console.log("post /updateCredentials");
+	// console.log("Password MD5: " + req.body.password.);
 
-	//console.log("Password MD5: " + req.body.password.);
-
-	admin.auth().updateUser(req.decodedClaims.uid, {
-		email: req.body.email,
-		password: req.body.password
-	})
-	.then(function(userRecord) {
-		console.log("User credentials correctly updated: " + userRecord);
-		res.end(JSON.stringify({ status: "success" }));
-	})
-	.catch(function (error) {
-		console.log('Error updating user credentials: ' + error);
-		res.status(500).send("Error Ocurred!");
-	});
+	admin
+		.auth()
+		.updateUser(req.decodedClaims.uid, {
+			email: req.body.email,
+			password: req.body.password
+		})
+		.then((userRecord) => {
+			console.log("/updateCredentials: User credentials correctly updated: " + userRecord);
+			res.status(200).end();
+		})
+		.catch((error) => {
+			console.log('/updateCredentials: Error updating user credentials: ' + error);
+			res.status(500).send("Error updating user credentials!");
+		});
 });
 
-router.get("/getUsers", verifySession, function (req, res) {
-	console.log("/getUsers");
+router.post("/getUsers", verifySession, (req, res) => {
+	console.log("post /getUsers");
 	listAllUsers(res, []);
 });
 
@@ -238,40 +217,22 @@ function listAllUsers (res, result, nextPageToken) {
 		.auth()
 		.listUsers(1000, nextPageToken)
 		.then((listUsersResult) => {
-
 			result = result.concat(listUsersResult.users);
 			if (listUsersResult.pageToken) {
 				listAllUsers(res, result, listUsersResult.pageToken);
 			} else {
-				console.log("Result: " + JSON.stringify(result));
 				res.status(200).setHeader('Content-Type', 'application/json');
 				res.json(result).end();
 			}
 		})
 		.catch((error) => {
-			console.log('Error listing users:', error);
-			res.status(500).send("Error ocurred!");
+			console.log('/listAllUsers: Error listing users:', error);
+			res.status(500).send("Error listing users!");
 		});
 };
 
-router.post("/deleteUser", verifySession, function (req, res) {
-	console.log("/deleteUser");
-
-	admin
-	.auth()
-	.deleteUser(req.body.uid)
-	.then(() => {
-		db.collection("userData").doc(req.body.uid).delete();
-		res.status(200).end();
-	})
-	.catch((err) => {
-		console.log('Error deleting user: ', error);
-		res.status(500).send("Error ocurred!");
-	});
-});
-
 router.post("/accountDetails", verifySession, function (req, res) {
-	console.log("/accountDetails");
+	console.log("post /accountDetails");
 
 	admin
 	.auth()
@@ -289,95 +250,81 @@ router.post("/accountDetails", verifySession, function (req, res) {
 					'postalCode': doc.data().postalCode,
 					'isAdmin': doc.data().isAdmin
 				};
-				console.log("Result: " + JSON.stringify(result));
 				res.status(200).setHeader('Content-Type', 'application/json');
 				res.send(JSON.stringify(result));
 			}
 		})
-		.catch(err => {
-			console.log("Error accountDetails " + err);
-			res.status(500).send("Error ocurred!");
+		.catch(error => {
+			console.log("/accountDetails: Error getting account details " + error);
+			res.status(500).send("Error getting account details!");
 		});
 	})
 	.catch((error) => {
-		console.log('Error fetching user data:', error);
-	});
-});
-
-router.post("/disabledUser", verifySession, function (req, res) {
-	console.log("/disabledUser");
-
-	admin
-		.auth()
-		.updateUser(req.body.uid, {
-			disabled: req.body.disabled,
-		})
-	.then((userRecord) => {
-		console.log('Successfully updated user', userRecord.toJSON());
-		res.status(200).end();
-	})
-	.catch((error) => {
-		console.log('Error updating user:', error);
-		res.status(500).end();
+		console.log('/accountDetails: Error getting user data:', error);
 	});
 });
 
 router.post("/verifyEmail", verifySession, function (req, res) {
-	console.log("/verifyEmail");
+	console.log("post /verifyEmail");
 
 	admin
 		.auth()
 		.updateUser(req.body.uid, {
 			emailVerified: true
 		})
-	.then((userRecord) => {
-		console.log('Successfully updated user', userRecord.toJSON());
-		res.status(200).end();
-	})
-	.catch((error) => {
-		console.log('Error updating user:', error);
-		res.status(500).end();
-	});
-});
-
-function verifySession(req, res, next) {
-	const sessionCookie = req.cookies.session || "";
-	admin
-		.auth()
-		.verifySessionCookie(sessionCookie, true)
-		.then((decodedClaims) => {
-			req.decodedClaims = decodedClaims;
-			next();
+		.then((userRecord) => {
+			console.log('/verifyEmail: User updated successfully', userRecord.toJSON());
+			res.status(200).end();
 		})
 		.catch((error) => {
-			console.log("error authentication: " + error);
-			res.status(500).send("Error Ocurred!");
+			console.log('/verifyEmail: Error updating user:', error);
+			res.status(500).end();
 		});
-}
+});
 
-function checkCookieMiddleware(req, res, next) {
-	console.log("checkCookieMiddleware");
-	const sessionCookie = req.cookies.session || '';
+router.post("/disabledUser", verifySession, function (req, res) {
+	console.log("post /disabledUser");
 
 	admin
 		.auth()
-		.verifySessionCookie(sessionCookie, true)
-		.then((decodedClaims) => {
-			console.log("checkCookieMiddleware next()");
-			req.decodedClaims = decodedClaims;
-			next();
+		.updateUser(req.body.uid, {
+			disabled: req.body.disabled,
 		})
-		.catch(error => {
-			console.log("checkCookieMiddleware login()");
-			res.render('login');
+		.then((userRecord) => {
+			console.log('/disabledUser: User updated successfully', userRecord.toJSON());
+			res.status(200).end();
 		})
-}
+		.catch((error) => {
+			console.log('/disabledUser: Error updating user:', error);
+			res.status(500).end();
+		});
+});
 
+router.post("/deleteUser", verifySession, function (req, res) {
+	console.log("post /deleteUser");
+
+	admin
+		.auth()
+		.deleteUser(req.body.uid)
+		.then((userRecord) => {
+			db.collection("userData").doc(req.body.uid).delete()
+				.then(() => {
+					console.log('/deleteUser: User deleted successfully', userRecord.toJSON());
+					res.status(200).end();
+				})
+				.catch(() => {
+					console.log('/deleteUser: Error deleting user: ', error);
+					res.status(500).send("Error deleting user from userData collection!");
+				});
+		})
+		.catch((err) => {
+			console.log('/deleteUser: Error deleting user: ', error);
+			res.status(500).send("Error ocurred!");
+		});
+});
 
 router.get("/analytics", (req, res) => {
-	console.log("get analytics");
-
-	const propertyId = 252994745;
+	console.log("get /analytics");
 
 	async function runReport() {
 		const [response] = await client.runReport({
@@ -416,35 +363,70 @@ router.get("/analytics", (req, res) => {
 	  });	  
 });
 
-/*router.post('/contact', (req, res) => {
-	console.log("Contact");
+router.post('/contact', (req, res) => {
+	console.log("post /contact");
 
-  const smtpTrans = nodemailer.createTransport({
-    host: 'mx4.compsaonline.com', //server
-    port: 587,
-    secure: false,
-    auth: {
-      user: "web@compsa.es", //email
-      pass: "121512xjsm" //password
-    }
-  })
-  // Specify what the email will look like
-  const mailOpts = {
-    from: 'Your sender info here', // This is ignored by Gmail
-    to: 'albertperezdatsira@gmail.com',
-    subject: 'New message from contact form at healthsites',
-    text: `${req.body.name} (${req.body.email}) subject: ${req.body.subject} says: ${req.body.message}`
-  }
+	const smtpTrans = nodemailer.createTransport({
+		host: process.env.MAIL_SERVER, //server
+		port: 587,
+		secure: false,
+		auth: {
+		user: process.env.MAIL_SERVER_USER, //email
+		pass: process.env.MAIL_SERVER_PASSWORD //password
+		}
+	})
 
-  // Attempt to send the email
-  smtpTrans.sendMail(mailOpts, (error, response) => {
-    if (error) {
-      res.render('contactError', { title: 'Contact Error' }) // Show a page indicating failure
-    }
-    else {
-      res.render('contactSuccess', { title: 'Contact Success' }) // Show a page indicating success
-    }
-  })
-})*/
+	// specify what the email will look like
+	const mailOpts = {
+		from: 'Your sender info here', // this is ignored by Gmail
+		to: 'albertperezdatsira@gmail.com',
+		subject: 'New message from contact form at ReciclameWeb',
+		text: `${req.body.name} (${req.body.email}) subject: ${req.body.subject} says: ${req.body.message}`
+	}
+
+	// attempt to send the email
+	smtpTrans.sendMail(mailOpts, (error, response) => {
+		if (error) {
+		res.render('contactError', { title: 'Contact Error' }) // show a page indicating failure
+		}
+		else {
+		res.render('contactSuccess', { title: 'Contact Success' }) // show a page indicating success
+		}
+	})
+})
+
+function verifySession(req, res, next) {
+	const sessionCookie = req.cookies.session || "";
+
+	admin
+		.auth()
+		.verifySessionCookie(sessionCookie, true)
+		.then((decodedClaims) => {
+			console.log("verifySession next()");
+			req.decodedClaims = decodedClaims;
+			next();
+		})
+		.catch((error) => {
+			console.log("Error verifySession: " + error);
+			res.status(500).send("Error verify session!");
+		});
+}
+
+function checkCookieMiddleware(req, res, next) {
+	const sessionCookie = req.cookies.session || '';
+
+	admin
+		.auth()
+		.verifySessionCookie(sessionCookie, true)
+		.then((decodedClaims) => {
+			console.log("checkCookieMiddleware next()");
+			req.decodedClaims = decodedClaims;
+			next();
+		})
+		.catch(error => {
+			console.log("Error checkCookieMiddleware, login redirect: " + error);
+			res.render('login');
+		})
+}
 
 module.exports = router;
